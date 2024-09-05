2d2010db20e42f5233422374fd61bc76b34a65d6
John Klint <gusjohn25@student.gu.se>
2024-09-05 21:43:41 +0200

    Last assignment of 0

--- assignments/0/naive_benchmarking.c
+++ assignments/0/naive_benchmarking.c
@@ -1,1 +1,20 @@
-asd
+#include <stdio.h>
+#include <time.h>
+
+int main(int) {
+  clock_t start = clock();
+  clock_t end;
+  long int sum;
+  float seconds;
+
+  const int iterations = 1E9;
+
+  for (long int ix = 0; ix < iterations; ++ix) {
+    sum += ix;
+  }
+  end = clock();
+  seconds = (float)(end - start) / CLOCKS_PER_SEC;
+  printf("The sum is %ld.\n", sum);
+  printf("The program took %f seconds to run.\n", seconds);
+}
+
