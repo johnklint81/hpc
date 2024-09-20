a0ef7aa6e36b88de4ee9b3ecf4f949ac63aa70b4
John Klint <gusjohn25@student.gu.se>
2024-09-20 13:36:35 +0200

    read_write: fixes

--- assignments/1/read_write.c
+++ assignments/1/read_write.c
@@ -11,12 +11,15 @@ int main(void) {
   int *data = malloc(n * sizeof(int));
 
   clock_gettime(CLOCK_MONOTONIC, &bench_start_time);
-  FILE *file = fopen("data.bin", "wr");
+  FILE *file = fopen("data.bin", "wb");
   for (int i = 0; i < n; i++) {
     fwrite(&i, sizeof(int), 1, file);  // fwrite is faster and binary, fprint is slower
     fflush(file);
   }
+  fclose(file);
   int i = 0;
+
+  file = fopen("data.bin", "rb");
   while (fread(&data[i], sizeof(int), 1, file) == 1) {
     i++;
   } 
@@ -33,11 +36,17 @@ int main(void) {
   for (int i = 0; i < n; i++) {
     data[i] = i;
   }
-  fwrite(data, sizeof(int), sizeof(data), file);
+  file = fopen("data.bin", "wb");
+  fwrite(data, sizeof(int), n, file);
+  fclose(file);
+  
+  file = fopen("data.bin", "rb");
   fread(data, sizeof(int), n, file);
+  fclose(file);
   clock_gettime(CLOCK_MONOTONIC, &bench_stop_time);
   bench_diff_time = (difftime(bench_stop_time.tv_sec, bench_start_time.tv_sec) 
     * 1000000 + (bench_stop_time.tv_nsec - bench_start_time.tv_nsec) / 1000) 
     / 1E6;
   printf("Writing and reading full array: %f\n", bench_diff_time);
+  free(data);
 }
